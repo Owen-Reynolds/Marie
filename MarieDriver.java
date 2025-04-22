7750d907e3e830e66a54b7742552b4fd291ee218
OwenxOwen <oreynold@ashland.edu>
2025-04-22 10:44:52 -0400

    Debugging Methods

--- MarieDriver.java
+++ MarieDriver.java
@@ -2,9 +2,18 @@ import java.util.*;
 
 public class MarieDriver {
     public static void main(String[] args) {
+
         //Creates scanner object to get user input
         Scanner sc = new Scanner(System.in);
 
-        
+        //Creates a MarieOperations object to use the methods
+        MarieOperations marie = new MarieOperations();
+        marie.input(5);
+        marie.store(0);
+        marie.input(10);
+        marie.store(1);
+        marie.load(0);
+        marie.add(1);
+        marie.output();
     }
 }
