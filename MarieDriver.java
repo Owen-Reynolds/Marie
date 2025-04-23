1442dc74ea288aa9a5dc2d8bf9ff51919d1ebde9
OwenxOwen <oreynold@ashland.edu>
2025-04-23 09:18:33 -0400

    Small Formatting Changes

--- MarieDriver.java
+++ MarieDriver.java
@@ -6,15 +6,16 @@ public class MarieDriver {
         
         //Sample program loading a program
         int[] program = {
-            0b010100000000, // 5 0  -> input()
-            0b001000001010, // 2 10 -> store 10
-            0b010100000000, // 5 0  -> input()
-            0b001100001010, // 3 10 -> add 10
-            0b011000000000, // 6 0  -> output()
-            0b111100000000  // F 0  -> halt
+            0b010100000000, // Opcode 5 Value 10 -> input()
+            0b001000001010, // Opcode 2 Value 10 -> store 10
+            0b010100000000, // Opcode 5 Value 10 -> input()
+            0b001100001010, // Opcode 3 Value 20 -> add 10
+            0b011000000000, // Opcode 6 Value 20 -> output()
+            0b111100000000  // -> halt
         };
-
+        //Loading the program into memory
         marie.loadProgram(program);
+        //
         marie.runProgram();
     }
 }
